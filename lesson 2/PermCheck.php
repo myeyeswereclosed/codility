857ad8aac73289fc3ea9bccb376ccd449eadc2fa
myeyeswereclosed <belovi-tech@mail.ru>
2015-08-05 13:22:48 +0300

    Add comments to code

--- lesson 2/PermCheck.php
+++ lesson 2/PermCheck.php
@@ -1,9 +1,11 @@
 <?php
 
 function solution($A) {
+    // Проверка, нет ли повторяющихся элементов в массиве
     $uniqueA = array_unique($A);
     if (count($uniqueA) != count($A))
         return 0;
+    // Если повторов нет, сверяем сумму массива и сумму чисел до наибольшего в массиве     
     else {
         $n = count($A);
         $sum = $n*($n+1)/2;
