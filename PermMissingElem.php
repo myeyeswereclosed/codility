b58ef81648fcae1dee8fd3a85a6729e662daad80
myeyeswereclosed <belovi-tech@mail.ru>
2015-08-05 11:04:31 +0300

    Delete PermMissingElem.php

--- PermMissingElem.php
+++
@@ -1,9 +0,0 @@
-<?php
-function solution($A) {
-    // write your code in PHP5.5
-    $n = count($A)+1;
-    $fullArraySum = $n*($n+1)/2;
-    $arraySum = array_sum($A);
-    return ($fullArraySum-$arraySum);
-}
-?>
